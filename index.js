887825a28a2cf8cb472510185d8b7d8e6ef657c0
orbit0801 <orbit0801@gmail.com>
2019-06-10 11:52:41 +0900

    refactoring varibale names.

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const Themeparks = require('themeparks');
 const fs = require('fs');
 
-const getPark = url =>
-  url === 'tds'
+const Park = {
+  tdl: 'tdl',
+  tds: 'tds'
+};
+
+const getParkObject = parkName =>
+  parkName === Park.tds
     ? new Themeparks.Parks.TokyoDisneyResortDisneySea()
     : new Themeparks.Parks.TokyoDisneyResortMagicKingdom();
 
 (async () => {
-  const tdl = getPark('tdl');
+  const tdl = getParkObject(Park.tdl);
   await fs.writeFile(
     '/var/www/html/tdl.json',
     JSON.stringify(await tdl.GetWaitTimes()),
     () => {}
   );
 
-  const tds = getPark('tds');
+  const tds = getParkObject(Park.tds);
   await fs.writeFile(
     '/var/www/html/tds.json',
     JSON.stringify(await tds.GetWaitTimes()),
